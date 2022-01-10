0f870be461f7adcac80164ff753eb629b2c9f143
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-01-10 15:16:04 +0200

    add new files

---
+++ src/FirstTree.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use function Php\Immutable\Fs\Trees\trees\mkdir;
+use function Php\Immutable\Fs\Trees\trees\mkfile;
+
+// mkdir вторым параметром принимает список детей
+// которые могут быть либо директориями созданными mkdir
+// либо файлами созданными mkfile
+
+$tree = Php\Immutable\Fs\Trees\trees\mkdir('etc', [
+  mkfile('bashrc'),
+  mkdir('consul', [
+    mkfile('config.json'),
+  ]),
+], ['key' => 'value']);
+
+
+
+print_r($tree);
